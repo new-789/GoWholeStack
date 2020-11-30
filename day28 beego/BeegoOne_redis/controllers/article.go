ffdb5f200d1337d0e28a19569671eb56a3eb9d67
奋斗的菜鸟 <44946039+new-789@users.noreply.github.com>
2020-11-30 19:00:15 +0800

    Update article.go

--- day28 beego/BeegoOne_redis/controllers/article.go
+++ day28 beego/BeegoOne_redis/controllers/article.go
@@ -471,7 +471,7 @@ func (this *ArticleController) DelArticleType() {
 // 发送邮件功能
 func (this *ArticleController) SendMail() {
 	// 定义邮件的配置信息
-	config := `{"username":"2314574867@qq.com",
+	config := `{"username":"yourMail@explame.com",
 				"password":"zagljrwdiggmdjgi",
 				"host":"smtp.qq.com",
 				"port":587
@@ -479,9 +479,9 @@ func (this *ArticleController) SendMail() {
 	// 获取邮箱实体
 	email := utils.NewEMail(config)
 	// 设置发送邮件的地址
-	email.From = "2314574867@qq.com"
+	email.From = "Mail@explame.com"
 	// 设置邮件接收地址,值为字符串切片格式，切片中可以写多个接收地址用来群发
-	email.To = []string{"zhuxiujian@foxmail.com"}
+	email.To = []string{"xxxxx@xxx.xx"}
 	// 设置邮件标题
 	email.Subject = "某某操作系统激活账号邮件"
 	// 设置邮件内容
