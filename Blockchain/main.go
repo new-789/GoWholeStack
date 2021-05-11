81d37a5eb1e9fb9b1c221af9a710acefd966db9b
new-789 <zhuxiujian@foxmail.com>
2021-05-11 23:00:03 +0800

    02_实现SetHash

--- Blockchain/main.go
+++ Blockchain/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"crypto/sha256"
+	"fmt"
+)
 
 //Block 1. 定义区块链结构
 type Block struct {
@@ -13,13 +16,24 @@ type Block struct {
 }
 //NewBlock 2. 创建区块
 func NewBlock(data string, precHash []byte) *Block {
-	return &Block{
+
+	block := &Block{
 		PrevHash: precHash,
 		Hash:     []byte{}, // 先填空，后面计算,TODO
 		Data:     []byte(data),
 	}
+	block.SetHash()
+	return block
 }
-//3. 生成哈希
+// SetHash 3. 生成哈希
+func (b *Block)SetHash() {
+	// TODO
+	// 1. 拼装数据，使用
+	blockInfo := append(b.PrevHash, b.Data...)
+	// 2. sha256
+	hash := sha256.Sum256(blockInfo)
+	b.Hash = hash[:]
+}
 //4. 引入区块链
 //5. 添加区块
 //6. 重构代码
