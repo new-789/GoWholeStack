39e8f5bc085bb379b2983891e26e855aa1f5ed37
new-789 <zhuxiujian@foxmail.com>
2021-05-12 19:37:07 +0800

    06_添加完整的区块字段数据并引用

--- Blockchain/block.go
+++ Blockchain/block.go
@@ -1,23 +1,51 @@
 package main
 
-import "crypto/sha256"
+import (
+	"crypto/sha256"
+	"time"
+)
 
 //Block 1. 定义区块链结构
 type Block struct {
-	//	 前区块哈希
+	// 1 版本号
+	Version uint64
+	// 2 前区块哈希
 	PrevHash []byte
-	//	 当前区块哈希
+	// 3 Merkel 根（梅克尔根，这就是一个哈希值，先不管，后面介绍）
+	MerkelRoot []byte
+	// 4 时间戳
+	TimeStamp uint64
+	// 5 难度值
+	Difficulty uint64
+	// 6 随机数，以为就是挖矿要找的数据
+	Nonce uint64
+
+	// a 当前区块哈希，正常比特币区块中没有当前区块的哈希，为了实现方便做了简化
 	Hash []byte
-	//	 数据
+	// b 数据
 	Data []byte
+}
+
+//1. 补充区块字段
+//2. 重新极端哈希函数
+//3. 优化代码
+
+// Uint64ToByte 辅助函数，将 uint64 转成 []byte
+func Uint64ToByte(num uint64) []byte  {
+	// TODO
+	return []byte{}
 }
 
 //NewBlock 2. 创建区块
 func NewBlock(data string, precHash []byte) *Block {
-
 	block := &Block{
+		Version: 00,
 		PrevHash: precHash,
-		Hash:     []byte{}, // 先填空，后面计算,TODO
+		MerkelRoot: []byte{},
+		TimeStamp: uint64(time.Now().Unix()),
+		Difficulty: 0,  // 随便填写的无效值
+		Nonce: 0,  // 同上
+		Hash:     []byte{}, // 先填空，后面计算
 		Data:     []byte(data),
 	}
 	block.SetHash()
@@ -26,9 +54,15 @@ func NewBlock(data string, precHash []byte) *Block {
 
 // SetHash 3. 生成哈希
 func (b *Block)SetHash() {
-	// TODO
+	var blockInfo []byte
 	// 1. 拼装数据
-	blockInfo := append(b.PrevHash, b.Data...)
+	blockInfo = append(blockInfo, Uint64ToByte(b.Version)...)
+	blockInfo = append(blockInfo, b.PrevHash...)
+	blockInfo = append(blockInfo, b.MerkelRoot...)
+	blockInfo = append(blockInfo, Uint64ToByte(b.TimeStamp)...)
+	blockInfo = append(blockInfo, Uint64ToByte(b.Difficulty)...)
+	blockInfo = append(blockInfo, Uint64ToByte(b.Nonce)...)
+	blockInfo = append(blockInfo, b.Data...)
 	// 2. sha256 生成哈希值
 	hash := sha256.Sum256(blockInfo)
 	b.Hash = hash[:] // 给当前区块添加哈希值
